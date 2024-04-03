96fb372fcc65c0bd8ffc4eebdefa2be73d0b85de
Anthony Martin <anthonymmartin@hotmail.com>
2024-04-03 12:06:11 +0000

    update

--- core-tf/scripts/bigquery/elt_config.sql
+++ core-tf/scripts/bigquery/elt_config.sql
@@ -1,14 +1,14 @@
-CREATE TABLE `amm-elt-demo.adventureworks.elt_config`
+CREATE TABLE adventureworks.elt_config
 (
   sourceTableName STRING,
   targetTableName STRING
-)
+);
 
-INSERT INTO `amm-elt-demo.adventureworks.elt_config`
+INSERT INTO adventureworks.elt_config
 VALUES 
   ('Sales.SalesOrderHeader', 'SalesOrderHeader'), 
   ('Sales.SalesOrderDetail', 'SalesOrderDetail'),
   ('Sales.SalesTerritory', 'SalesTerritory'),
   ('Production.Product', 'Product'),
-  ('Production.ProductCategory', 'ProductCategory')
-  ('Production.ProductSubcategory', 'ProductSubcategory')
+  ('Production.ProductCategory', 'ProductCategory'),
+  ('Production.ProductSubcategory', 'ProductSubcategory');
